eb790bbe08dd8e598ed8c3e509d07666ad843a13
Juan Dalmasso <j_dalmasso@outlook.com>
2019-04-24 13:57:45 +1000

    Init repo, added highcharts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = {
+  title: {
+    text: 'Solar Employment Growth by Sector, 2010-2016'
+  },
+
+  subtitle: {
+      text: 'Source: thesolarfoundation.com'
+  },
+
+  yAxis: {
+      title: {
+          text: 'Number of Employees'
+      }
+  },
+  legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle'
+  },
+
+  plotOptions: {
+      series: {
+          label: {
+              connectorAllowed: false
+          },
+          pointStart: 2010
+      }
+  },
+
+  series: [{
+      name: 'Installation',
+      data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+  }, {
+      name: 'Manufacturing',
+      data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+  }, {
+      name: 'Sales & Distribution',
+      data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+  }, {
+      name: 'Project Development',
+      data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+  }, {
+      name: 'Other',
+      data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+  }],
+
+  responsive: {
+      rules: [{
+          condition: {
+              maxWidth: 500
+          },
+          chartOptions: {
+              legend: {
+                  layout: 'horizontal',
+                  align: 'center',
+                  verticalAlign: 'bottom'
+              }
+          }
+      }]
+  }
 }
+
+const App = () => (
+  <div className="App">
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+    />
+  </div>
+)
 
 export default App;
